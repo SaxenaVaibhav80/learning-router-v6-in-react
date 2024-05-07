1c2e61053edab8cc4d88784ac2a3e09fbbb04929
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-05-08 04:43:02 +0530

    learning conditional rendering in react

--- router/src/components/OtherPage.jsx
+++ router/src/components/OtherPage.jsx
@@ -2,7 +2,10 @@ import { useLocation } from "react-router-dom"
 function   OtherPage()
 {
     const location = useLocation()
-    console.log(location.state)
+
+    // applying ternary operator just for  learning --->
+
+    location==6 ? <h1>hello</h1>:<h1>heelo2</h1>
     return (
         <>
             <h1>hello this is other page with id = {location.state.my_id}</h1>
